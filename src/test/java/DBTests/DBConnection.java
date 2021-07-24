da1f7caed99a2770286366dbb8d18d35820dccca
Duotech Academy <dtacademyb5@gmail.com>
2021-07-24 12:08:22 -0400

    First Commit

---
+++ src/test/java/DBTests/DBConnection.java
@@ -0,0 +1,41 @@
+package DBTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+
+public class DBConnection {
+
+    @Test
+    public void connectToDb1(){
+        System.out.println("Db connection test 1");
+               Assert.assertTrue(true);
+    }
+
+    @Test
+    public void connectToDb2(){
+        System.out.println("Db connection test 2");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void connectToDb3(){
+        System.out.println("Db connection test 3");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void connectToDb4(){
+        System.out.println("Db connection test 4");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void TC001_DB(){
+        System.out.println("Db connection test 4");
+        Assert.assertTrue(true);
+    }
+}
