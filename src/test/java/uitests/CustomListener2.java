543609dfbe71f7bbb3f23972415eae1e59d0a8b7
Duotech <dtacademyb4@gmail.com>
2021-08-28 13:16:42 -0400

    new commit in the branch

---
+++ src/test/java/uitests/CustomListener2.java
@@ -0,0 +1,31 @@
+package uitests;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.time.LocalDateTime;
+
+public class CustomListener2 implements ITestListener {
+
+    public void onTestStart(ITestResult result) {
+        System.out.println("TEST: " + result.getName()+ " has started at " + LocalDateTime.now());
+
+    }
+
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("TEST: " + result.getName()+ " passed at " + LocalDateTime.now());
+
+    }
+
+    public void onTestFailure(ITestResult result) {
+        System.out.println("TEST: " + result.getName()+ " failed at " + LocalDateTime.now());
+    }
+
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("TEST: " + result.getName()+ " skipped at " + LocalDateTime.now());
+    }
+
+
+
+
+}
